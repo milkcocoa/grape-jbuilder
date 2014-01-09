ce295698316d244194f763dfba504033a532f975
Pat Allan <pat@freelancing-gods.com>
2014-01-09 13:59:43 +1100

    Formatter shifted so directories match namespacing.

--- lib/grape/jbuilder.rb
+++ lib/grape/jbuilder.rb
@@ -3,6 +3,7 @@ require 'hashie/hash'
 require 'jbuilder'
 require 'tilt'
 require 'tilt/jbuilder.rb'
-require 'grape/jbuilder/formatter'
+
+require 'grape/formatter/jbuilder'
 require 'grape/jbuilder/renderer'
 require 'grape/jbuilder/version'
