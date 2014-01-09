513d7ff1b812afacde131014b249db4ff3521e3c
Pat Allan <pat@freelancing-gods.com>
2014-01-09 14:16:21 +1100

    If subject is app, let's just use app.

--- spec/acceptance/partials_spec.rb
+++ spec/acceptance/partials_spec.rb
@@ -1,22 +1,18 @@
 require 'spec_helper'
 
 describe 'Grape::Jbuilder partials' do
-  subject do
-    Class.new(Grape::API)
-  end
+  let(:app) { Class.new(Grape::API) }
 
-  before do
-    subject.format :json
-    subject.formatter :json, Grape::Formatter::Jbuilder
-    subject.before { env['api.tilt.root'] = "#{File.dirname(__FILE__)}/../views" }
-  end
-
-  def app
-    subject
+  before :each do
+    app.format :json
+    app.formatter :json, Grape::Formatter::Jbuilder
+    app.before do
+      env['api.tilt.root'] = "#{File.dirname(__FILE__)}/../views"
+    end
   end
 
   it 'proper render partials' do
-    subject.get('/home', jbuilder: 'project') do
+    app.get('/home', jbuilder: 'project') do
       @author   = OpenStruct.new(author: 'LTe')
       @type     = OpenStruct.new(type: 'paper')
       @project  = OpenStruct.new(name: 'First', type: @type, author: @author)
--- spec/acceptance/templates_spec.rb
+++ spec/acceptance/templates_spec.rb
@@ -1,39 +1,34 @@
 require 'spec_helper'
 
 describe Grape::Jbuilder do
-  subject do
-    Class.new(Grape::API)
-  end
+  let(:app) { Class.new(Grape::API) }
 
   before do
-    subject.format :json
-    subject.formatter :json, Grape::Formatter::Jbuilder
-    subject.helpers RSpec::Mocks::ExampleMethods
-  end
-
-  def app
-    subject
+    app.format :json
+    app.formatter :json, Grape::Formatter::Jbuilder
+    app.helpers RSpec::Mocks::ExampleMethods
   end
 
   it 'should work without jbuilder template' do
-    subject.get('/home') { 'Hello World' }
+    app.get('/home') { 'Hello World' }
     get '/home'
     last_response.body.should == '"Hello World"'
   end
 
   it 'should raise error about root directory' do
-    subject.get('/home', jbuilder: true){}
+    app.get('/home', jbuilder: true){}
     expect { get '/home' }.to raise_error
   end
 
-
-  context 'titl root is setup'  do
-    before do
-      subject.before { env['api.tilt.root'] = "#{File.dirname(__FILE__)}/../views" }
+  context 'tilt root is setup'  do
+    before :each do
+      app.before do
+        env['api.tilt.root'] = "#{File.dirname(__FILE__)}/../views"
+      end
     end
 
     it 'should respond with proper content-type' do
-      subject.get('/home', jbuilder: 'user') {
+      app.get('/home', jbuilder: 'user') {
         @user    = double(name: 'Fred', email: 'fred@bloggs.com')
         @project = double(name: 'JBuilder')
       }
@@ -42,13 +37,13 @@ describe Grape::Jbuilder do
     end
 
     it 'should raise error with an invalid jbuilder value' do
-      subject.get('/home', jbuilder: true){}
+      app.get('/home', jbuilder: true){}
       expect { get '/home' }.to raise_error
     end
 
     ['user', 'user.jbuilder'].each do |jbuilder_option|
       it 'should render jbuilder template (#{jbuilder_option})' do
-        subject.get('/home', jbuilder: jbuilder_option) do
+        app.get('/home', jbuilder: jbuilder_option) do
           @user    = OpenStruct.new(name: 'LTe', email: 'email@example.com')
           @project = OpenStruct.new(name: 'First')
         end
